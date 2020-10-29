422b60d142383ddd4aeb531670101d1a7f93e8c5
Adnan Adiatman <adnan.adiatman@62teknologi.com>
2020-10-29 13:15:55 +0700

    20

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -21,4 +21,9 @@ RSpec.describe 'Card' do
   it 'has a suit' do
     expect(card.suit).to eq('Spades')
   end
+
+  it 'has a custom massage' do
+    comparison = 'Spade'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
+  end
 end
