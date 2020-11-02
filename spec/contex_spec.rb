56c57337eae393a871f0caa9a59c6733373d8673
Adnan Adiatman <adnan.adiatman@62teknologi.com>
2020-11-02 11:55:05 +0700

    22

--- spec/contex_spec.rb
+++ spec/contex_spec.rb
@@ -1,7 +1,18 @@
 require 'rspec'
 
 RSpec.describe '#even? method' do
-  it 'should return true if number is even'
+  # it 'should return true if number is even'
+  # it 'should return false if number is odd'
 
-  it 'should return false if number is odd'
+  describe 'with even number' do
+    it 'should return true' do
+      expect(4.even?).to eq(true)
+    end
+  end
+
+  describe 'with odd number' do
+    it 'should return false' do
+      expect(5.even?).to eq(false)
+    end
+  end
 end
