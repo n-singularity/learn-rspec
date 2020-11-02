487910793abc1aebbcf406d63e03bc5de61ffa7e
Adnan Adiatman <adnan.adiatman@62teknologi.com>
2020-11-02 12:12:07 +0700

    23

---
+++ spec/before_and_after_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+
+RSpec.describe 'before and after hooks' do
+  before(:context) do
+    puts 'Before context'
+  end
+
+  after(:context) do
+    puts 'After context'
+  end
+
+  before(:example) do
+    puts 'Before example'
+  end
+
+  after(:example) do
+    puts 'After example'
+  end
+
+  it 'is just  a random example' do
+    puts 'this example'
+    expect(5 * 4).to eq(20)
+  end
+
+  it 'is just another random example' do
+    puts 'this example'
+    expect(3 - 2).to eq(1)
+  end
+
+end
