a63643a0515c871fd65e37cef626d4c91f59422c
Adnan Adiatman <adnan.adiatman@62teknologi.com>
2020-11-04 07:56:02 +0700

    29

---
+++ spec/one_liner_syntax_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe 'shorted syntax' do
+  subject { 5 }
+
+  context 'with classic syntax' do
+    it 'should equal 5' do
+      expect(subject).to eq(5)
+    end
+  end
+
+  context 'with one-liner syntax' do
+    it { is_expected.to eq(5) }
+  end
+end
