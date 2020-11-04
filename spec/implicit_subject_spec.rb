5e148870782d192357b1ff093cd4dddbe94f1ec3
Adnan Adiatman <adnan.adiatman@62teknologi.com>
2020-11-04 07:27:00 +0700

    26

---
+++ spec/implicit_subject_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+
+RSpec.describe Hash do
+
+  it 'should start off empty' do
+    expect(subject.length).to eq(0)
+    subject[:some_key] = "Some Value"
+    expect(subject.length).to eq(1)
+  end
+
+  it 'is isolated between example' do
+    expect(subject.length).to eq(0)
+  end
+end
