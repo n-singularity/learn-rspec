04e3912d62ac0bdd7103bfe697e2398543a423de
Adnan Adiatman <adnan.adiatman@62teknologi.com>
2020-11-09 09:21:44 +0700

    34

--- spec/methods/equality_matchers_spec.rb
+++ spec/methods/equality_matchers_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'equality matcher' do
 
   # eql akan mengecek value
   describe 'eq matcher' do
-    it 'check for value, including same type' do
+    it 'check for value' do
       expect(a).to eq(3)
       expect(b).to eq(3.0)
       expect(a).to eq(b)
@@ -17,6 +17,19 @@ RSpec.describe 'equality matcher' do
       expect(a).not_to eql(3)
       expect(b).not_to eql(3.0)
       expect(a).not_to eql(b)
+    end
+  end
+
+  # equal atau be(alias) akan mengecek lokasi memory yang sama,
+  # meski value dan tipenya sama tapi jika lokasi memorynya berbeda maka akan dianggap berbeda
+  describe 'equal and be matcher' do
+    let(:c) { [1, 2, 3] }
+    let(:d) { [1, 2, 3] }
+    let(:e) { c }
+    it 'check for value, including same memory' do
+      expect(c).not_to equal(d)
+      expect(c).not_to equal([1, 2, 3])
+      expect(c).to equal(e)
     end
   end
 end
