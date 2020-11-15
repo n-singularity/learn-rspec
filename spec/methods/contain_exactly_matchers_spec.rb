dbc9679f35e79a69057051a11511a9932edd11ad
Adnan Adiatman <adnan.adiatman@62teknologi.com>
2020-11-15 16:08:23 +0700

    40

---
+++ spec/methods/contain_exactly_matchers_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+
+# yang dicek adalah perubahannya atau selisihnya
+RSpec.describe 'contain exactly matcher' do
+  subject { [1, 2, 3] }
+
+  describe 'long from syntax' do
+    it 'for check for the presence all the element' do
+      expect(subject).to contain_exactly(1, 2, 3)
+      expect(subject).to contain_exactly(2, 3, 1)
+      expect(subject).to contain_exactly(3, 2, 1)
+    end
+  end
+end
