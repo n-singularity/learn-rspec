859fd1a28aca2fc544297c848c1afa95bbda6e34
Adnan Adiatman <adnan.adiatman@62teknologi.com>
2020-11-20 17:08:24 +0700

    51

--- spec/mocking/movie_spec.rb
+++ spec/mocking/movie_spec.rb
@@ -12,11 +12,11 @@ class Actor
     'I Love you, baby'
   end
 
-  def fall_off_leader
+  def fall_off_ladder
     'Call my agent! No way!'
   end
 
-  def light_of_fire
+  def light_on_fire
     false
   end
 end
@@ -30,13 +30,28 @@ class Movie
 
   def start_shooting
     if actor.ready?
-      actor.act
-      actor.fall_off_leader
-      actor.light_of_fire
+      puts actor.act
+      puts actor.fall_off_ladder
+      puts actor.light_on_fire
     end
   end
 end
 
-actor = Actor.new('Brad Pitt')
-movie = Movie.new(actor)
-movie.start_shooting
+# actor = Actor.new('Brad Pitt')
+# movie = Movie.new(actor)
+# movie.start_shooting
+
+RSpec.describe Movie do
+  let(:stuntman) { double('Mr. Danger', ready?: true , act: 'Any String at all', fall_off_ladder: 'Sure! lets do it', light_on_fire: true) }
+  subject { described_class.new(stuntman) }
+
+  describe '#start_shooting method' do
+    it 'expect actor to do 3 action ' do
+      expect(stuntman).to receive(:ready?)
+      expect(stuntman).to receive(:act)
+      expect(stuntman).to receive(:fall_off_ladder)
+      expect(stuntman).to receive(:light_on_fire)
+      subject.start_shooting
+    end
+  end
+end
